c40ca4d74ab5bf492a2fc9f67ade3ee77699f1cb
Trubo <78948819+truubo@users.noreply.github.com>
2024-12-02 21:09:07 -0600

    new logo, projects page more complete

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const myName = "James"
-const greetings = ["Hi, I'm ", "Kamusta, ako si ", "Hola, soy ", "Hallo, ich bin "]
+const greetings = [
+    "Hi, I'm ", // english
+    "Kamusta, ako si ", // tagalog
+    "Hola, soy ", // spanish
+    "Hallo, ich bin ", // german
+    "Olá, meu nome é " // portuguese
+]
 
-document.querySelector(".home").querySelector("#greeting").innerHTML = greetings[
+document.querySelector(".animated-page").querySelector("#greeting").innerHTML = greetings[
     Math.floor(Math.random() * greetings.length)
     ] + myName + "."
